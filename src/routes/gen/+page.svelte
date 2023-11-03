4c53b9eaf3772fdb301bfb8dc1246833e4a452d9
DairyLand <farmerhunter@proton.me>
2023-11-03 14:33:35 -0400

    models

--- src/routes/gen/+page.svelte
+++ src/routes/gen/+page.svelte
@@ -8,11 +8,9 @@
 	<label class="label">
 		<span>Models</span>
 		<select class="select">
-			<option value="1">Option 1</option>
-			<option value="2">Option 2</option>
-			<option value="3">Option 3</option>
-			<option value="4">Option 4</option>
-			<option value="5">Option 5</option>
+			<option value="1">Stable XL</option>
+			<option value="2">Barbie</option>
+			<option value="3">GTA</option>
 		</select>
 	</label>
 
