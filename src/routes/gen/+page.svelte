2b117a0b88dcf0b7c4c8b9c7ae2e1805f7cecfc3
DairyLand <farmerhunter@proton.me>
2023-11-03 14:32:38 -0400

    texct area

--- src/routes/gen/+page.svelte
+++ src/routes/gen/+page.svelte
@@ -1,2 +1,20 @@
 <!-- this file will contain the ui for the image gen the js for the replicate should go in the server js-->
+<div>
+	<label class="label">
+		<span>Input</span>
+		<input class="input" type="text" placeholder="Your Prompt here" />
+	</label>
+
+	<label class="label">
+		<span>Models</span>
+		<select class="select">
+			<option value="1">Option 1</option>
+			<option value="2">Option 2</option>
+			<option value="3">Option 3</option>
+			<option value="4">Option 4</option>
+			<option value="5">Option 5</option>
+		</select>
+	</label>
+
+</div>
   
