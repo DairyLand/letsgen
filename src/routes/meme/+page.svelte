cb874343c7ebfc5d2c03d8991c6a7e2e6a2eb2ad
DairyLand <farmerhunter@proton.me>
2023-11-03 15:50:16 -0400

    working meme api

---
+++ src/routes/meme/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	let memeData = null;
+  
+	async function fetchRandomMeme() {
+	  try {
+		const response = await fetch('https://meme-api.com/gimme');
+		if (response.ok) {
+		  memeData = await response.json();
+		} else {
+		  console.error('Failed to fetch meme');
+		}
+	  } catch (error) {
+		console.error('Error fetching meme:', error);
+	  }
+	}
+  </script>
+  
+  <main>
+	<button on:click={fetchRandomMeme}>Summon Random Meme</button>
+  
+	{#if memeData}
+	  <div>
+		<img src={memeData.url} alt={memeData.title} />
+	  </div>
+	{:else}
+	  <p>Click the button to fetch a meme.</p>
+	{/if}
+  </main>
+  
